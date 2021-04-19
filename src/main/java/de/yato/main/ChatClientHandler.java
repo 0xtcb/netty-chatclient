c8fe2501b1c90c68ef5f3f7b666543398ac14133
Yato361 <63376584+Yato361@users.noreply.github.com>
2021-04-19 09:54:15 +0200

    Added chat client

---
+++ src/main/java/de/yato/main/ChatClientHandler.java
@@ -0,0 +1,13 @@
+package de.yato.main;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundMessageHandlerAdapter;
+
+public class ChatClientHandler extends ChannelInboundMessageHandlerAdapter<String> {
+
+    @Override
+    public void messageReceived(ChannelHandlerContext channelHandlerContext, String s) throws Exception {
+        System.out.println(s);
+    }
+
+}
